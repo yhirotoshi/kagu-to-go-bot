2b3582bd35891e847c0c375b7db53acc93e546e8
yhirotoshi <h.yanagi-95@outlook.jp>
2018-07-06 22:36:43 +0900

    自動応答に関する会話をlogging対象にした

--- conditions.py
+++ conditions.py
@@ -34,39 +34,44 @@ line_bot_api = LineBotApi(channel_secret)
 def kagu_select_conditions(event):
     if event.postback.data == 'ベッドを探す':
         logging_chat(event)
-        template_message = bed_size_check_flex_style()
+        message = bed_size_check_flex_style()
+        logging_auto_response(event, message.alt_text)
         line_bot_api.reply_message(
-            event.reply_token, template_message)
+            event.reply_token, message)
 
     if event.postback.data == 'ダイニングテーブルを探す':
         logging_chat(event)
-        template_message = diningtable_size_check_flex_style()
-        send_auto_response(event, confirm_message.alt_text)
+        message = diningtable_size_check_flex_style()
+        logging_auto_response(event, message.alt_text)
         line_bot_api.reply_message(
-            event.reply_token, template_message)
+            event.reply_token, message)
 
     if event.postback.data == 'ソファを探す':
         logging_chat(event)
-        template_message = sofa_size_check_flex_style()
+        message = sofa_size_check_flex_style()
+        logging_auto_response(event, message.alt_text)
         line_bot_api.reply_message(
-            event.reply_token, template_message)
+            event.reply_token, message)
 
     if event.postback.data == 'チェアを探す':
         logging_chat(event)
-        template_message = isu_price_select_flex_style()
+        message = isu_price_select_flex_style()
+        logging_auto_response(event, message.alt_text)
         line_bot_api.reply_message(
-            event.reply_token, template_message)
+            event.reply_token, message)
 
     if event.postback.data == 'ざっくり相談する':
         logging_chat(event)
-        template_message = confirm_room_style()
+        message = confirm_room_style()
+        logging_auto_response(event, message.alt_text)
         line_bot_api.reply_message(
-            event.reply_token, template_message)
+            event.reply_token, message)
 
 def size_select_conditions(event):
     if event.postback.data in ['シングル','セミダブル','ダブル','2人用', '4人用','6人用','1人用', '3人用','それ以上']:
         logging_chat(event)
         confirm_message = price_select_flex_style()
+        logging_auto_response(event, confirm_message.alt_text)
         line_bot_api.reply_message(
             event.reply_token, confirm_message)
 
@@ -75,6 +80,7 @@ def price_select_conditions(event):
     if event.postback.data in ['~1万円','1~2万円','2~3万円','3~5万円', '1~3万円','3~5万円','5~10万円', '10万円~',]:
         logging_chat(event)
         confirm_message = confirm_room_style_flex_style()
+        logging_auto_response(event, confirm_message.alt_text)
         line_bot_api.reply_message(
             event.reply_token, confirm_message)
 
@@ -82,11 +88,13 @@ def price_select_conditions(event):
 def image_upload_conditions(event):
     if event.postback.data in ['部屋の写真がある', '家具のイメージ写真がある']:
         logging_chat(event)
-         line_bot_api.reply_message(
-           event.reply_token, TextSendMessage(text='画像をお見せいただけますか？'))
+        logging_auto_response(event, '画像をお見せいただけますか？')
+        line_bot_api.reply_message(
+          event.reply_token, TextSendMessage(text='画像をお見せいただけますか？'))
     elif event.postback.data == '選択肢から選ぶ':
         logging_chat(event)
         confirm_message = room_style_select()
+        logging_auto_response(event, confirm_message.alt_text)
         line_bot_api.reply_message(
             event.reply_token, confirm_message)
     elif event.postback.data == '特にない':
@@ -97,16 +105,19 @@ def image_upload_conditions(event):
 def image_add_conditions(event):
     if event.postback.data == 'まだある':
         logging_chat(event)
+        logging_auto_response(event, '画像をお送りください')
         line_bot_api.reply_message(
            event.reply_token, TextSendMessage(text='画像をお送りください'))
     
     elif event.postback.data == 'もう大丈夫':
         logging_chat(event)
+        logging_auto_response(event, '入力お疲れ様でした~~~')
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text='入力お疲れ様でした. \nコンシェルジュがお客様にぴったりな家具をお探ししますので少々お待ち下さい.\n何か他にご要望ございましたらご自由にお申し付けください.'))
 
 def room_style_select_conditions(event):
     if event.postback.data in ['ナチュラル・北欧スタイル', 'カジュアルスタイル', 'モダンスタイル']:
         logging_chat(event)
+        logging_auto_response(event, '入力お疲れ様でした~~~')
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text='入力お疲れ様でした. \nコンシェルジュがお客様にぴったりな家具をお探ししますので少々お待ち下さい.\n何か他にご要望ございましたらご自由にお申し付けください.'))
